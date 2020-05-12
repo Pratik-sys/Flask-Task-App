749f7529b162e9f260f176280cd635ca553b0c37
Pratik-sys <pithore.pratik@gmail.com>
2020-05-12 18:50:24 +0530

    create sqlite database

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from flask import Flask, render_template,url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 
 app = Flask(__name__) 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db' # set the congif where you want to save the db 
+db = SQLAlchemy(app)
+
+class Todo(db.Model): 
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
 
 @app.route('/')
 def index():
@@ -8,4 +21,4 @@ def index():
     
 
 if __name__ == "__main__":
-    app.run(debug=True,  port=8888)
+    app.run(debug=True,  port=5050)
