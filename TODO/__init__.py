8358c046bc997e0713c4d86590312d7fd289f664
Pratik-Sys <pithore.pratik@gmail.com>
2020-08-24 08:48:05 +0530

    init the bcrypt
    Signed-off-by: Pratik-Sys <pithore.pratik@gmail.com>

--- TODO/__init__.py
+++ TODO/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
 
 app = Flask(__name__) 
 app.config['SECRET_KEY'] = 'ce93c0d66f7ea57ff4c6'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Todo.db' # set the congif where you want to save the db 
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from Todo import routes 
