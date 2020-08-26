697bf34bba734f57bd8867a88f7d32fd5f45dc50
Pratik-Sys <pithore.pratik@gmail.com>
2020-08-26 09:57:18 +0530

    generate registration and login form
    Signed-off-by: Pratik-Sys <pithore.pratik@gmail.com>

---
+++ TODO/forms.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField, TextField, validators
+from Todo.models import User
+
+class RegistrtionForm(FlaskForm):
+    username = StringField('Username', [validators.data_required(), validators.Length(max=25)])
+    email = StringField('Email', [validators.data_required()])
+    password = PasswordField('Password', [validators.data_required()])
+    submit = SubmitField('Sign up')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', [validators.data_required()])
+    password = PasswordField('Password', [validators.data_required()])
+    submit = SubmitField('Login')
+
