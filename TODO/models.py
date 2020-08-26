a20218d450ac4e14cc47970b6537bd58a9c5af9e
Pratik-Sys <pithore.pratik@gmail.com>
2020-08-26 09:56:22 +0530

    Create User table
    Signed-off-by: Pratik-Sys <pithore.pratik@gmail.com>

--- TODO/models.py
+++ TODO/models.py
@@ -10,3 +10,17 @@ class Todo(db.Model):
     def __repr__(self):
         return f'{self.id}'
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(200), nullable=False, unique=True)
+    email = db.Column(db.String(200), nullable=False, unique=True)
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f'{self.username}, {self.email}'
+
+
+
+
+
+
