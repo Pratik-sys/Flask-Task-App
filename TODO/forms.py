6e0956f1e5bfbe5b7f0800f95f10831534cf0a30
Pratik-Sys <pithore.pratik@gmail.com>
2020-08-28 16:35:12 +0530

    Add error handling to signup and login section
    Signed-off-by: Pratik-Sys <pithore.pratik@gmail.com>

--- TODO/forms.py
+++ TODO/forms.py
@@ -1,15 +1,25 @@
 # from flask_wtf import FlaskForm
-from wtforms import Form, StringField, SubmitField, PasswordField, TextField, validators
+from wtforms import Form, StringField, SubmitField, PasswordField, TextField, validators, ValidationError
 from Todo.models import User
 
 class RegistrationForm(Form):
     username = StringField('Username', [validators.data_required(), validators.Length(max=25)])
-    email = StringField('Email', [validators.data_required()])
+    email = StringField('Email', [validators.data_required(), validators.Email()])
     password = PasswordField('Password', [validators.data_required()])
     submit = SubmitField('Sign up')
 
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken. Please choose a different one.')
+
 class LoginForm(Form):
-    email = StringField('Email', [validators.data_required()])
+    email = StringField('Email', [validators.data_required(), validators.Email()])
     password = PasswordField('Password', [validators.data_required()])
     submit = SubmitField('Login')
 
