4ff0cd1862a77ce2f6ec42bc5876e4f73dfec9eb
Pratik-Sys <pithore.pratik@gmail.com>
2021-06-01 21:31:48 +0530

    supress sql warning
    __init__.py:872: FSADeprecationWarning: SQLALCHEMY_TRACK_MODIFICATIONS adds significant overhead and will be disabled by default in the future.  Set it to True or False to suppress this warning.

--- Todo/__init__.py
+++ Todo/__init__.py
@@ -7,6 +7,7 @@ from flask_login import LoginManager
 app = Flask(__name__) 
 app.config['SECRET_KEY'] = 'ce93c0d66f7ea57ff4c6'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Todo.db' # set the congif where you want to save the db 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
