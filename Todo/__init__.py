e672e27b2588b18fa5e650601851ffb03ea8e3a0
Pratik-Sys <pithore.pratik@gmail.com>
2021-08-31 22:32:10 +0530

    move configs in the config file

--- Todo/__init__.py
+++ Todo/__init__.py
@@ -2,22 +2,30 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from Todo.config import Config
 
 
-app = Flask(__name__) 
-app.config['SECRET_KEY'] = 'ce93c0d66f7ea57ff4c6'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Todo.db' # set the congif where you want to save the db 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
 
-from Todo.users.routes import users
-from Todo.todo.routes import todos
-from Todo.main.routes import main
 
-app.register_blueprint(users)
-app.register_blueprint(todos)
-app.register_blueprint(main)
+
+def create_app(config_class = Config):
+    app = Flask(__name__) 
+    app.config.from_object(Config)
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+
+    from Todo.users.routes import users
+    from Todo.todo.routes import todos
+    from Todo.main.routes import main
+
+    app.register_blueprint(users)
+    app.register_blueprint(todos)
+    app.register_blueprint(main)
+
+    return app
--- run.py
+++ run.py
@@ -1,4 +1,6 @@
-from Todo import app 
+from Todo import create_app
+
+app = create_app()
 
 if __name__ == "__main__":
     app.run(debug=True,  port=5050)
